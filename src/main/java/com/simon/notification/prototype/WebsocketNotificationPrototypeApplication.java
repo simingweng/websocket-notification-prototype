377ae95ded5f5c26fc7a2de55f0a60e58b8b6cc6
Simon Weng <wen55527@jdsu-mbp.ds.jdsu.net>
2017-07-20 13:55:50 -0400

    publish dummy notification messages to 2 different destinations periodically

--- src/main/java/com/simon/notification/prototype/WebsocketNotificationPrototypeApplication.java
+++ src/main/java/com/simon/notification/prototype/WebsocketNotificationPrototypeApplication.java
@@ -1,12 +1,48 @@
 package com.simon.notification.prototype;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.broker.BrokerAvailabilityEvent;
+
+import javax.annotation.PreDestroy;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class WebsocketNotificationPrototypeApplication {
+    private static final Logger logger = LoggerFactory.getLogger(WebsocketNotificationPrototypeApplication.class);
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private ScheduledFuture scheduledFuture;
 
     public static void main(String[] args) {
         SpringApplication.run(WebsocketNotificationPrototypeApplication.class, args);
     }
+
+    @EventListener
+    public void onBrokerAvailabilityChanged(BrokerAvailabilityEvent event) {
+        if (event.isBrokerAvailable()) {
+            scheduledFuture = Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(() -> {
+                logger.info("broadcasting messages");
+                simpMessagingTemplate.convertAndSend("/topic/demotenant1", "Hello, demo tenant1, this is an event for you!");
+                simpMessagingTemplate.convertAndSend("/topic/demotenant2", "Hello, demo tenant2, this is an event for you!");
+            }, 0, 3, TimeUnit.SECONDS);
+        } else {
+            stopGreeting();
+        }
+    }
+
+    @PreDestroy
+    public void stopGreeting() {
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(true);
+        }
+    }
+
 }
