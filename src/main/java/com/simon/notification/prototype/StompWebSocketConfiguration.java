77638a59cb40feb6f216b629c45fe2dbca741bcf
SiMing Weng <siming.weng@gmail.com>
2017-07-20 08:34:31 -0400

    initial commit

---
+++ src/main/java/com/simon/notification/prototype/StompWebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.simon.notification.prototype;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/notification");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        super.configureMessageBroker(registry);
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableStompBrokerRelay("/topic");
+    }
+}
---
+++ src/main/java/com/simon/notification/prototype/WebsocketNotificationPrototypeApplication.java
@@ -0,0 +1,12 @@
+package com.simon.notification.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketNotificationPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketNotificationPrototypeApplication.class, args);
+    }
+}
